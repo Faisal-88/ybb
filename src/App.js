2456bef08354443bdb67ad62066319a619e11026
faisaltimbangan32@gmail.com <faisaltimbangan32@gmail.com>
2022-03-19 01:36:10 +0700

    add halo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Halo GenerasiGIGIH 2.0</p>
+        <h2>M Faisal</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
